b6f8ec51fccd969931a9893acddbde8fd4781cda
Uselessface <Uselessface@mail.ru>
2021-02-16 03:34:59 +0400

    tried oop, didnt help much

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 window.onload = function() {
-	document.querySelectorAll('[data-toggle="tab"]').forEach(tab => {
-  	tab.onclick = function(e) {
-    	const tabId = this.getAttribute('href');
-      document.querySelectorAll('.tabs > div').forEach(tab => {
-      	tab.classList.remove('active');
-      });
-      console.log(document.querySelector(tabId));
-      document.querySelector(tabId).classList.add('active');
-      e.preventDefault();
-      return false;
-    };
-  });
+    function _(selector) {
+	    const arr = Array.from(document.querySelectorAll(selector));
+      return arr.length === 1 ? arr[0] : arr;
+    }
 
-  function _(selector) {
-	  const arr = Array.from(document.querySelectorAll(selector));
-    return arr.length === 1 ? arr[0] : arr;
-  }
+
+  	_('[data-toggle="tab"]').forEach(tab => {
+  	  tab.onclick = function(e) {
+        const tabId = this.getAttribute('href');
+        _('.tabs > div').forEach(tab => {
+          tab.classList.remove('active');
+        });
+        console.log(_(tabId));
+        _(tabId).classList.add('active');
+        e.preventDefault();
+        return false;
+     };
+   });
+
 
   const url = 'https://apiinterns.osora.ru/';
-
   const form = _('#form_upload');
 
   form.onsubmit = function(u) {
     u.preventDefault()
 
-    const files = document.querySelector('[type=file]').files
+    const files = _('[type=file]').files
     const formData = new FormData()
 
     for (let i = 0; i < files.length; i++) {
@@ -41,34 +42,70 @@ window.onload = function() {
     }).then((response) => {
       console.log(response)
     })
-
+    
 
   }
-  
-  const bookName = _('#bookName');
-  const area = _('#area');
-  const book__name = _('.book__name');
-  const book__content = _('.book__content');
-
-  bookName.value = localStorage.getItem('bookName');  
-  bookName.oninput = () => {
-    localStorage.setItem('bookName' , bookName.value);
-    book__name.innerHTML = bookName.value;
-  };
-  book__name.innerHTML = localStorage.getItem('bookName');
-
-  area.value = localStorage.getItem('area');  
-  area.oninput = () => {
-    localStorage.setItem('area' , area.value);
-    book__content.innerHTML = area.value;
-  };
-  book__content.innerHTML = localStorage.getItem('area');
-  
-  
 
   
+  class Book {
+    constructor(title,content) {
+      this.title = title;
+      this.content = content;
+    }
+  }
+
+  class UI {
+    static displayBooks() {
+      const books = Store.getBooks();
+
+      books.forEach((book) => UI.addBookToList(book));
+      }
+
+    static addBookToList(book) {
+      const list = _('.book-list');
+      const li = document.createElement('li');
+
+      li.innerHTML = `${book.title}`;
+
+      list.appendChild(li);
+      }
+
+    static clearFields() {
+      _('#title').value = '';
+      _('#content').value = '';
+      }
+    }
+
+    class Store {
+      static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+          books = [];
+        } else {
+          books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+      }
+
+      static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+
+    }
 
 
+
+    _('#save').onclick = function(e) {
+        const title = document.querySelector('#title').value;
+        const book = new Book(title, content);
+        UI.addBookToList(book);
+        Store.addBook(book);
+        UI.clearFields(); 
+    } 
+    document.addEventListener('DOMContentLoaded', UI.displayBooks);
 }
-
-
+           
+    
